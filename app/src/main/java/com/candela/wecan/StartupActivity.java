453833540804775dca50dfe22afd78082ad3465a
shivam <shivam.thakur@candelatech.com>
2021-12-23 14:44:17 +0530

    added video player
    Signed-off-by: shivam <shivam.thakur@candelatech.com>

--- app/src/main/java/com/candela/wecan/StartupActivity.java
+++ app/src/main/java/com/candela/wecan/StartupActivity.java
@@ -59,58 +59,60 @@ public class StartupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
         getSupportActionBar().hide();
-        button = (Button) findViewById(R.id.enter_button);
-        server_ip = findViewById(R.id.ip_enter_page);
-        u_name = findViewById(R.id.user_name);
-        test_name_tv = findViewById(R.id.test_name);
-        sharedpreferences = getBaseContext().getSharedPreferences("userdata", Context.MODE_PRIVATE);
-        Map<String,?> keys = sharedpreferences.getAll();
-        String last_ip = (String) keys.get("last");
-        String user_name = (String) keys.get("user_name");
-        String test_name = (String) keys.get("test_name");
-        server_ip.setText(last_ip);
-        u_name.setText(user_name);
-        test_name_tv.setText(test_name);
-        checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION, 1);
-        checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, 2);
-        checkPermission(Manifest.permission.ACCESS_WIFI_STATE, 3);
-        checkPermission(Manifest.permission.ACCESS_NETWORK_STATE, 4);
-        checkPermission(Manifest.permission.CHANGE_WIFI_STATE, 5);
-//        Intent myIntent = new Intent(this, ClientConnectivityConfiguration.class);
-//        startActivity(myIntent);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (keys.keySet().contains(server_ip.getText().toString())){
-                    Map<String,?> keys = sharedpreferences.getAll();
-                    String ip = server_ip.getText().toString();
-                    String realm_id = (String) keys.get("realm-" + ip);
-                    String resource_id = (String) keys.get("resource-" + ip);
-                    SharedPreferences.Editor editor = sharedpreferences.edit();
-                    editor.putString("user_name" ,  u_name.getText().toString());
-                    editor.putString("test_name" ,  test_name_tv.getText().toString());
-                    editor.apply();
-                    editor.commit();
-                    connect_server(server_ip.getText().toString(), resource_id, realm_id, view);
-                }
-                else{
-                    SharedPreferences.Editor editor = sharedpreferences.edit();
-                    editor.putString(server_ip.getText().toString(), "-1");
-                    editor.putString("resource-" + server_ip.getText().toString(), "-1");
-                    editor.putString("realm-" + server_ip.getText().toString(), "-1");
-                    editor.putString("user_name" ,  u_name.getText().toString());
-                    editor.putString("test_name" ,  test_name_tv.getText().toString());
-                    editor.apply();
-                    editor.commit();
-                    Map<String,?> keys = sharedpreferences.getAll();
-                    String ip = server_ip.getText().toString();
-                    String realm_id = (String) keys.get("realm-" + ip);
-                    String resource_id = (String) keys.get("resource-" + ip);
-                    connect_server(server_ip.getText().toString(), resource_id, realm_id, view);
-                }
-            }
-        });
+        Intent myIntent = new Intent(this, VideoStream.class);
+        startActivity(myIntent);
+//        button = (Button) findViewById(R.id.enter_button);
+//        server_ip = findViewById(R.id.ip_enter_page);
+//        u_name = findViewById(R.id.user_name);
+//        test_name_tv = findViewById(R.id.test_name);
+//        sharedpreferences = getBaseContext().getSharedPreferences("userdata", Context.MODE_PRIVATE);
+//        Map<String,?> keys = sharedpreferences.getAll();
+//        String last_ip = (String) keys.get("last");
+//        String user_name = (String) keys.get("user_name");
+//        String test_name = (String) keys.get("test_name");
+//        server_ip.setText(last_ip);
+//        u_name.setText(user_name);
+//        test_name_tv.setText(test_name);
+//        checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION, 1);
+//        checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, 2);
+//        checkPermission(Manifest.permission.ACCESS_WIFI_STATE, 3);
+//        checkPermission(Manifest.permission.ACCESS_NETWORK_STATE, 4);
+//        checkPermission(Manifest.permission.CHANGE_WIFI_STATE, 5);
+////        Intent myIntent = new Intent(this, ClientConnectivityConfiguration.class);
+////        startActivity(myIntent);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                if (keys.keySet().contains(server_ip.getText().toString())){
+//                    Map<String,?> keys = sharedpreferences.getAll();
+//                    String ip = server_ip.getText().toString();
+//                    String realm_id = (String) keys.get("realm-" + ip);
+//                    String resource_id = (String) keys.get("resource-" + ip);
+//                    SharedPreferences.Editor editor = sharedpreferences.edit();
+//                    editor.putString("user_name" ,  u_name.getText().toString());
+//                    editor.putString("test_name" ,  test_name_tv.getText().toString());
+//                    editor.apply();
+//                    editor.commit();
+//                    connect_server(server_ip.getText().toString(), resource_id, realm_id, view);
+//                }
+//                else{
+//                    SharedPreferences.Editor editor = sharedpreferences.edit();
+//                    editor.putString(server_ip.getText().toString(), "-1");
+//                    editor.putString("resource-" + server_ip.getText().toString(), "-1");
+//                    editor.putString("realm-" + server_ip.getText().toString(), "-1");
+//                    editor.putString("user_name" ,  u_name.getText().toString());
+//                    editor.putString("test_name" ,  test_name_tv.getText().toString());
+//                    editor.apply();
+//                    editor.commit();
+//                    Map<String,?> keys = sharedpreferences.getAll();
+//                    String ip = server_ip.getText().toString();
+//                    String realm_id = (String) keys.get("realm-" + ip);
+//                    String resource_id = (String) keys.get("resource-" + ip);
+//                    connect_server(server_ip.getText().toString(), resource_id, realm_id, view);
+//                }
+//            }
+//        });
     }
 
     public void openServerConnection () {
